6457338b584995b78c185f7d6c288d3175f48b33
Jason Coward <jason@modx.com>
2011-03-27 21:42:03 -0600

    Add changelog to transport package

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -70,6 +70,7 @@ $builder->putVehicle($vehicle);
 $builder->setPackageAttributes(array(
     'license' => file_get_contents($sources['source_core'] . '/docs/license.txt'),
     'readme' => file_get_contents($sources['source_core'] . '/docs/readme.txt'),
+    'changelog' => file_get_contents($sources['source_core'] . '/docs/changelog.txt'),
 ));
 
 /* zip up the package */
