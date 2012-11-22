bb085c2e0542c4a03622c4190a4026b576d5f272
Jason Coward <jason@opengeek.com>
2012-11-21 19:57:26 -0700

    Add tplWrapper for specifying a wrapper template
    Also adds wrapIfEmpty property to optionally apply the wrapper if the getResources output is empty

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -31,7 +31,7 @@ $modx->setLogTarget(XPDO_CLI_MODE ? 'ECHO' : 'HTML');
 
 /* set package info */
 define('PKG_NAME','getresources');
-define('PKG_VERSION','1.5.2');
+define('PKG_VERSION','1.6.0');
 define('PKG_RELEASE','dev');
 
 /* load builder */
--- core/components/getresources/docs/readme.txt
+++ core/components/getresources/docs/readme.txt
@@ -1,7 +1,7 @@
 --------------------
 Snippet: getResources
 --------------------
-Version: 1.5.1-pl
+Version: 1.6.0-dev
 Released: August 23, 2012
 Since: December 28, 2009
 Author: Jason Coward <jason@modx.com>
